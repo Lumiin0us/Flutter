5f4a9110328dc33e0bb9064e08f1770bf05252cf
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-08-04 23:54:41 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/util/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.background,
+        appBar: AppBar(
+          backgroundColor: AppColors.primary,
+        ),
+        body: const Text("Text"),
+      ),
+    );
+  }
+}
---
+++ util/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors 
+{
+  // static const Color primary = Color(0xff212121);
+  // static const Color secondary = Color(0xffFFC107);
+  // static const Color background = Color(0xff0D47A1);
+  static const Color background = Color(0xFF000000); // Deep Space Black
+  static const Color primary = Color(0xFF003366); // Galactic Blue
+  static const Color secondary = Color(0xFF8E44AD); // Nebula Purple
+  static const Color textAndUI = Color(0xFFECF0F1); // Cosmic White
+  static const Color highlightsAndIcons = Color(0xFFFFD700); // Starlight Gold
+  static const Color contrastAndAction = Color(0xFF27AE60); // Aurora Green
+}
